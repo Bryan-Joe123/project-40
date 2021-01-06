7e398c99fe65f60e445f9d2cac9315daedd2fd4a
Bryan-Joe123 <75519189+Bryan-Joe123@users.noreply.github.com>
2021-01-06 20:22:54 +0400

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyChmVqEVAbSCLa8OpC06kqeSn55CgK5b5M",
+authDomain: "project-40-14862.firebaseapp.com",
+databaseURL: "https://project-40-14862-default-rtdb.firebaseio.com",
+projectId: "project-40-14862",
+storageBucket: "project-40-14862.appspot.com",
+messagingSenderId: "957463750918",
+appId: "1:957463750918:web:282510839f0b965e0c5687"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
